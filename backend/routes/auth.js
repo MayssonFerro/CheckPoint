f6c323070d804c4234d9638634e37ed4d9379f3e
Maysson Ferro <mayssonalexandre@gmail.com>
2025-12-08 20:06:50 -0400

    done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,7 +10,7 @@ router.post('/register', async (req, res) => {
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: 'Usuário já existe' });
     }
 
     const newUser = new User({
